3ccd03c3cdac9af9d372ec113b03fb1c5da2bb4f
Nir Benor <benornir1@gmail.com>
2019-08-24 17:39:56 +0300

    Grid search if params are not passed

--- sagol/models/bagging_regressor.py
+++ sagol/models/bagging_regressor.py
@@ -1,9 +1,20 @@
 import numpy as np
 from sklearn.ensemble import BaggingRegressor
+from sklearn.model_selection import GridSearchCV
 
 
 def train_bagging_regressor(x_train: np.ndarray, y_train: np.ndarray, **kwargs) -> BaggingRegressor:
     # Parallelize training on all CPUs.
     mdl = BaggingRegressor(n_jobs=-1, verbose=True, **kwargs)
-    mdl.fit(x_train, y_train)
-    return mdl
+
+    if 'n_estimators' in kwargs:
+        mdl.fit(x_train, y_train)
+        return mdl
+    else:
+        param_grid = {
+            'n_estimators': [5, 10, 20, 50, 100, 250],
+        }
+        print(f'n_estimators for `bagging_regressor` not passed, performing grid search. Using param_grid: {param_grid} '
+              f'This may take a while...')
+        gs = GridSearchCV(estimator=mdl, param_grid=param_grid)
+        return gs.best_estimator_
--- sagol/models/svr.py
+++ sagol/models/svr.py
@@ -1,9 +1,32 @@
 import numpy as np
+from sklearn.model_selection import GridSearchCV
 
 from sklearn.svm import SVR
 
 
 def train_svr(x_train: np.ndarray, y_train: np.ndarray, **kwargs) -> SVR:
     mdl = SVR(verbose=True, **kwargs)
-    mdl.fit(x_train, y_train)
-    return mdl
+
+    should_grid_search = False
+    param_grid = {}
+
+    if 'C' not in kwargs:
+        should_grid_search = True
+        param_grid['C'] = [0.1, 1, 10, 100, 250]
+    if 'gamma' not in kwargs:
+        should_grid_search = True
+        param_grid['gamma'] = [1e-5, 1e-4, 1e-3, 0.01, 0.1, 0.25]
+    if 'kernel' not in kwargs:
+        should_grid_search = True
+        param_grid['kernel'] = ['rbf', 'linear', 'poly']
+
+    if should_grid_search:
+        param_grid['degree'] = [2]
+
+        print(f'Performing grid search. Using param_grid: {param_grid} '
+              f'This may take a while...')
+        gs = GridSearchCV(estimator=mdl, param_grid=param_grid)
+        return gs.best_estimator_
+    else:
+        mdl.fit(x_train, y_train)
+        return mdl
