8180f31ace7a449e2242d97a6c14fc5ee682e622
Amit Bergman <t-amberm@microsoft.com>
2019-08-31 14:42:18 +0300

    changes

--- sagol/gui/app.py
+++ sagol/gui/app.py
@@ -6,9 +6,7 @@ from sagol.utils import setup_log_handlers
 setup_log_handlers()
 
 root = tk.Tk()
-MAIN_WINDOW = tk.Frame(root)
-MAIN_WINDOW.pack()
 
-load_initial_window(MAIN_WINDOW)
+load_initial_window(root)
 
 root.mainloop()
